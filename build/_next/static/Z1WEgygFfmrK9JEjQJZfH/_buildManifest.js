b0199e7916853e82e6d49824f340db3da2b81148
knowlesc <knowlesc8@gmail.com>
2022-11-09 10:49:47 -0800

    Update artifacts

--- build/_next/static/Z1WEgygFfmrK9JEjQJZfH/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/207-bf002baa31f4c8f2.js","static/chunks/pages/index-fb68e4338a612ca3.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
